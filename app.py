c9bc09051bf40f909208aae788338a1b37e10365
Wizz0 <samgorokhov01@gmail.com>
2025-03-15 15:13:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    message = 'Добро пожаловать!'
+    message = 'Добро пожаловать! version 2.0'
     return render_template('index.html', message=message)
